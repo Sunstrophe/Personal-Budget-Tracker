936e5152a3dd21cf862c236f8a99c8d11b701a6b
Sunny <mackansund_2@live.se>
2023-09-17 23:55:19 +0200

    Undone changes for logging

---
+++ json_log.py
@@ -0,0 +1,22 @@
+import json
+
+
+
+def read_userfiles(username_files):
+    new_username_files = json.dump(username_files)
+
+
+    return new_username_files
+
+def log_user(username, password):
+    new_username_files = 
+    username_files = {
+        "username" : username,
+        "password" : password
+    }
+
+    
+
+
+def log_expenses():
+    pass
